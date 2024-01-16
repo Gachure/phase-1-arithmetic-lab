7c50ae59ece12e1b3a9dbd6f587a838f36e8fb4e
Gachure <your.email@example.com>
2024-01-16 07:48:10 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply); // Output should be 62
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random); // Output should be a random integer greater than 0
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+console.log(mod); // Output should be 4
+let max = Math.max(10, 15, 20, 5);
+console.log(max); // Output should be 20
